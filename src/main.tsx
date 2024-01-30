938433285cc9101aa9f04a7a961679dc07d4b8f7
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-01-29 22:24:04 -0300

    feat: create dir's to project and its architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <ToastContainer theme="dark" autoClose={3 * 1000} />
+    </BrowserRouter>
+  </React.StrictMode>
+);
